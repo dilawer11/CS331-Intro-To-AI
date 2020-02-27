16b870bb8981e5e6806f0fa7f220a01b5278e5d1
Dilawer Ahmed <dilawer11@gmail.com>
2020-02-27 21:17:50 +0500

    Complete

--- A1/part2/robot/genetic_algorithm.py
+++ A1/part2/robot/genetic_algorithm.py
@@ -4,6 +4,7 @@ import random
 from collections import Counter
 from itertools import chain
 from bitstring import *
+import numpy as np
 
 ########################################################
 '''
@@ -19,7 +20,7 @@ from bitstring import *
 
 class GeneticAlgorithm(object):
 
-    def __init__(self, POPULATION_SIZE, CHROMOSOME_LENGTH, verbose):
+    def __init__(self, POPULATION_SIZE, CHROMOSOME_LENGTH, verbose, testing=False):
         self.wall_bit_string_raw = "01010101011001101101010111011001100101010101100101010101"
         self.wall_bit_string = ConstBitStream(bin = self.wall_bit_string_raw)
         self.population_size = POPULATION_SIZE
@@ -51,14 +52,9 @@ class GeneticAlgorithm(object):
             pairs_of_parents = self.select_parents(parents)
 
             # Recombination of pairs.
-            recombinded_parents = list(chain(*map(lambda pair: \
-                self.recombine_pairs_of_parents(pair[0], pair[1]), \
-                    pairs_of_parents))) 
-
+            recombinded_parents = list(chain(*map(lambda pair: self.recombine_pairs_of_parents(pair[0], pair[1]), pairs_of_parents))) 
             # Mutation of each individual
-            mutated_offspring = list(map(lambda offspring: \
-                self.mutate_offspring(offspring), recombinded_parents))
-
+            mutated_offspring = list(map(lambda offspring: self.mutate_offspring(offspring), recombinded_parents))
             # Evaluation of individuals
             parents = self.evaluate_candidates(mutated_offspring) # new parents (offspring)
             if self.verbose and not self.terminate:
@@ -100,7 +96,11 @@ class GeneticAlgorithm(object):
         '''
 
         # **** Start of Your Code **** #
-        pass
+        random.shuffle(parents)
+        pairs = []
+        for i in range(0, len(parents), 2):
+            pairs.append((parents[i], parents[i+1]))
+        return pairs
         # **** End of Your Code **** #
 
 
@@ -122,7 +122,14 @@ class GeneticAlgorithm(object):
         '''
 
         # **** Start of Your Code **** #
-        pass
+        candidate_sols = []
+        for _ in range(n):
+            str_raw = ""
+            for _ in range(56):
+                str_raw += str(random.choice([0,1]))
+            temp = ConstBitStream(bin=str_raw)
+            candidate_sols.append(temp)
+        return candidate_sols
         # **** End of Your Code **** # 
 
     def recombine_pairs_of_parents(self, p1, p2):
@@ -132,9 +139,15 @@ class GeneticAlgorithm(object):
 
         split at .6-.9 of 56 bits (CHROMOSOME_LENGTH). i.e. between 31-50 bits
         """
-
         # **** Start of Your Code **** #
-        pass
+        point = random.randrange(31,50)
+        p1_bits = p1.bin
+        p2_bits = p2.bin
+        c1_bits = p1_bits[:point] + p2_bits[point:]
+        c2_bits = p2_bits[:point] + p1_bits[point:]
+        p1 = ConstBitStream(bin=c1_bits)
+        p2 = ConstBitStream(bin=c2_bits)
+        return p1, p2
         # **** End of Your Code **** #
 
     def mutate_offspring(self, p):
@@ -144,8 +157,40 @@ class GeneticAlgorithm(object):
         '''
 
         # **** Start of Your Code **** #
-        pass
+        p_bits = p.bin
+        prob = (1 / self.chromosome_length)
+        weights = [prob, 1-prob]
+        m_bits = ""
+        for i in range(len(p_bits)):
+            flip = random.choices([True, False], weights=weights)[0]
+            if flip:
+                if p_bits[i] == '1':
+                    bit = '0'
+                else:
+                    bit = '1'
+            else:
+                bit = p_bits[i]
+            m_bits += bit
+        m = ConstBitStream(bin=m_bits)
+        return m
         # **** End of Your Code **** #
+    def fitness_of_one_individual(self, _candidate_sol):
+        _WallBitString = self.wall_bit_string
+        _WallBitString.pos = 0
+        _candidate_sol.pos = 0
+        
+        matching_bit_pairs = 0
+        try:
+            if not self.terminate:
+                while (_WallBitString.read(2).bin == _candidate_sol.read(2).bin):
+                    matching_bit_pairs = matching_bit_pairs + 1
+                if matching_bit_pairs >= 28:
+                    self.terminate = True
+                else:
+                    return matching_bit_pairs
+        except: # When all bits matched. 
+            self.terminate = True
+            return 28
 
     def evaluate_candidates(self, candidates): 
         '''
@@ -155,9 +200,16 @@ class GeneticAlgorithm(object):
                     but elements are not unique. Fittest candidates will have multiple copies.
                     Size of 'parents' must be equal to population size.  
         '''
-
         # **** Start of Your Code **** #
-        pass
+        
+        fitness = {}
+        for candidate in candidates:
+            fitness[candidate] = self.fitness_of_one_individual(candidate)
+        lst = list(fitness.keys())
+        weights= list(fitness.values())
+        print('Max Fitness:', np.max(fitness.values()))
+        parents = random.choices(lst, weights, k=len(candidates))
+        return parents
         # **** End of Your Code **** # 
 
 
